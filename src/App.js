1b522dda4f19ebb6f944a6810dc0f4d14398413d
Eileen Rhodes <69407155+etrhodes@users.noreply.github.com>
2020-12-06 08:52:02 -0700

    new files

--- src/App.js
+++ src/App.js
@@ -1,14 +1,45 @@
 import React from 'react';
-import TheDate from './state/TheDate';
-import Counter from './state/Counter';
+import Tabs from './state/Tabs';
 
 class App extends React.Component {
+  static defaultProps = { 
+    tabs: [
+    { name: 'First tab',
+    content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque.' },
+  { name: 'Second tab',
+    content: 'Laboriosam exercitationem quos consectetur expedita consequatur. Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit.' },
+  { name: 'Third tab',
+    content: 'Fugit, sapiente aspernatur corporis velit, dolor eum reprehenderit provident ipsam, maiores incidunt repellat! Facilis, neque doloremque. Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam exercitationem quos consectetur expedita consequatur.' },
+
+  ],
+};
+  state = {currentTabIndex: 0 
+  };
+  renderButtons() {
+    return this.props.tabs.map((tab, index) => (
+    <button key={index} onClick={() => this.handleButtonClick(index)}>
+      {tab.name}
+    </button>
+    ))
+  }
+  renderContent() {
+    const currentTab = this.props.tabs[this.state.currentTabIndex]
+    return (
+      <div className='content'>
+        {currentTab.content}
+      </div>
+    )
+  }
+  handleButtonClick(index) {
+    console.log('button clicked!', { index });
+    this.setState({ currentTabIndex: index })
+  }
   render() {
     return (
-      <main className='App'>
-        <TheDate />
-        <Counter count={123} step={1}/>
-      </main>
+      <div>
+        {this.renderButtons()}
+        {this.props.tabs.length && this.renderContent()}
+        </div>
     )
   }
 }
